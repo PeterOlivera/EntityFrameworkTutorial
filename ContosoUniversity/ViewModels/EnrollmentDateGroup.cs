b0cecb1117983c7e52272a97035fa89077faf371
Peter Olivera <polivera71@hotmail.com>
2017-05-20 01:00:59 -0400

    Added View Model for Enrollment Date grouping

---
+++ ContosoUniversity/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        public DateTime? EnrollmentDate { get; set; }
+
+        [Display(Name = "Students")]
+        public int StudentCount { get; set; }
+    }
+}
